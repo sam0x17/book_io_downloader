71a143758649485f65524e01a9819e0f52992b08
Sam Johnson <sam@durosoft.com>
2023-10-31 18:13:11 -0400

    WIP

--- src/client.rs
+++ src/client.rs
@@ -1,21 +1,20 @@
+use blockfrost::{load, BlockFrostApi};
 use ipfs_api::IpfsClient;
-use std::string::FromUtf8Error;
 
 pub const METADATA_URL: &'static str = "https://api.book.io/api/v0/collections";
 
 #[derive(Clone)]
 pub struct Client {
     ipfs_client: IpfsClient,
+    blockfrost_client: BlockFrostApi,
     metadata_url: &'static str,
 }
 
-impl Default for Client {
-    fn default() -> Self {
-        Self {
-            ipfs_client: IpfsClient::default(),
-            metadata_url: METADATA_URL,
-        }
-    }
+fn build_block_frost_api(project_id: &str) -> blockfrost::Result<BlockFrostApi> {
+    let configurations = load::configurations_from_env()?;
+    let project_id = configurations[project_id].as_str().unwrap();
+    let api = BlockFrostApi::new(project_id, Default::default());
+    Ok(api)
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
@@ -26,9 +25,31 @@ pub enum GetMetadataError {
     ToDo,
 }
 
+#[derive(Debug)]
+pub enum CreateClientError {
+    MissingBlockFrostProjectId,
+    BlockFrostError(blockfrost::Error),
+}
+
+impl From<blockfrost::Error> for CreateClientError {
+    fn from(value: blockfrost::Error) -> Self {
+        CreateClientError::BlockFrostError(value)
+    }
+}
+
 impl Client {
-    pub fn new() -> Self {
-        Client::default()
+    pub fn new(
+        block_frost_project_id: Option<&'static str>,
+        metadata_url: Option<&'static str>,
+    ) -> Result<Self, CreateClientError> {
+        Ok(Client {
+            ipfs_client: IpfsClient::default(),
+            blockfrost_client: build_block_frost_api(
+                block_frost_project_id.ok_or(CreateClientError::MissingBlockFrostProjectId)?,
+            )?
+            .into(),
+            metadata_url: metadata_url.unwrap_or(METADATA_URL),
+        })
     }
 
     pub async fn get_metadata(&self) -> Result<CollectionMetadata, GetMetadataError> {
