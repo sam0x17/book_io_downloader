7bef8ed98dc28526409b2cc0fa44571882e50d8a
Sam Johnson <sam@durosoft.com>
2023-10-31 01:11:18 -0400

    hyper stuff but needs rework using blockfrost

--- src/client.rs
+++ src/client.rs
@@ -1,17 +1,23 @@
-use hyper::{client::HttpConnector, Body};
+use std::string::FromUtf8Error;
+
+use hyper::{body::to_bytes, client::HttpConnector, Body, Uri};
 use ipfs_api::IpfsClient;
+
+pub const METADATA_URL: &'static str = "https://api.book.io/api/v0/collections";
 
 #[derive(Clone)]
 pub struct Client {
     ipfs_client: IpfsClient,
-    hyper_client: hyper::Client<HttpConnector, Body>,
+    http_client: hyper::Client<HttpConnector, Body>,
+    metadata_url: &'static str,
 }
 
 impl Default for Client {
     fn default() -> Self {
         Self {
             ipfs_client: IpfsClient::default(),
-            hyper_client: hyper::Client::default(),
+            http_client: hyper::Client::default(),
+            metadata_url: METADATA_URL,
         }
     }
 }
@@ -19,12 +25,35 @@ impl Default for Client {
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct CollectionMetadata;
 
+#[derive(Debug)]
+pub enum GetMetadataError {
+    Network(hyper::Error),
+    MalformedResponseUtf8(FromUtf8Error),
+}
+
+impl From<hyper::Error> for GetMetadataError {
+    fn from(value: hyper::Error) -> Self {
+        GetMetadataError::Network(value)
+    }
+}
+
+impl From<FromUtf8Error> for GetMetadataError {
+    fn from(value: FromUtf8Error) -> Self {
+        GetMetadataError::MalformedResponseUtf8(value)
+    }
+}
+
 impl Client {
     pub fn new() -> Self {
         Client::default()
     }
 
-    pub async fn get_collection_metadata(&self) -> Result<CollectionMetadata, ()> {
+    pub async fn get_metadata(&self) -> Result<CollectionMetadata, GetMetadataError> {
+        let future = self.http_client.get(Uri::from_static(self.metadata_url));
+        let response = future.await?;
+        let body_bytes = to_bytes(response.into_body()).await?;
+        let body_string = String::from_utf8(body_bytes.to_vec())?;
+        println!("{body_string}");
         Ok(CollectionMetadata)
     }
 }
