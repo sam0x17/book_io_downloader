12cc785cb63bea27e2397ca8c14ba11a3f18e153
Sam Johnson <sam@durosoft.com>
2023-10-30 22:29:10 -0400

    basic client setup

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,20 @@
-pub struct Client;
+use ipfs_api::IpfsClient;
+
+#[derive(Clone)]
+pub struct Client {
+    ipfs_client: IpfsClient,
+}
+
+impl Default for Client {
+    fn default() -> Self {
+        Self {
+            ipfs_client: IpfsClient::default(),
+        }
+    }
+}
+
+impl Client {
+    pub fn new() -> Self {
+        Client::default()
+    }
+}
