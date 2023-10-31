3a0ab3f8bc79b41b43b9c40eb80a4d99eda926c5
Sam Johnson <sam@durosoft.com>
2023-10-31 16:31:15 -0400

    rip out hyper because we need to use blockfrost

--- src/client.rs
+++ src/client.rs
@@ -1,14 +1,11 @@
-use std::string::FromUtf8Error;
-
-use hyper::{body::to_bytes, client::HttpConnector, Body, Uri};
 use ipfs_api::IpfsClient;
+use std::string::FromUtf8Error;
 
 pub const METADATA_URL: &'static str = "https://api.book.io/api/v0/collections";
 
 #[derive(Clone)]
 pub struct Client {
     ipfs_client: IpfsClient,
-    http_client: hyper::Client<HttpConnector, Body>,
     metadata_url: &'static str,
 }
 
@@ -16,7 +13,6 @@ impl Default for Client {
     fn default() -> Self {
         Self {
             ipfs_client: IpfsClient::default(),
-            http_client: hyper::Client::default(),
             metadata_url: METADATA_URL,
         }
     }
@@ -27,20 +23,7 @@ pub struct CollectionMetadata;
 
 #[derive(Debug)]
 pub enum GetMetadataError {
-    Network(hyper::Error),
-    MalformedResponseUtf8(FromUtf8Error),
-}
-
-impl From<hyper::Error> for GetMetadataError {
-    fn from(value: hyper::Error) -> Self {
-        GetMetadataError::Network(value)
-    }
-}
-
-impl From<FromUtf8Error> for GetMetadataError {
-    fn from(value: FromUtf8Error) -> Self {
-        GetMetadataError::MalformedResponseUtf8(value)
-    }
+    ToDo,
 }
 
 impl Client {
@@ -49,11 +32,6 @@ impl Client {
     }
 
     pub async fn get_metadata(&self) -> Result<CollectionMetadata, GetMetadataError> {
-        let future = self.http_client.get(Uri::from_static(self.metadata_url));
-        let response = future.await?;
-        let body_bytes = to_bytes(response.into_body()).await?;
-        let body_string = String::from_utf8(body_bytes.to_vec())?;
-        println!("{body_string}");
         Ok(CollectionMetadata)
     }
 }
